f410f80ad23e1a654a96da3608c09e58b174e598
Pradeep-joon <pradeepjoon0001@gmail.com>
2022-06-18 02:04:27 +0530

    Commit

--- datatypes.java
+++ datatypes.java
@@ -1,8 +1,16 @@
+
+import java.util.*;
 public class datatypes {
     public static void main(String[] args){
-      int a =25;
-      int b = 10;
-      int c = (a*b)/(a-b);
-      System.out.println(c);   
+      //int a =25;
+      //int b = 10;
+      //int c = (a*b)/(a-b);
+      //System.out.println(c);   
+  //User Input using Scanner :
+     Scanner scn  = new Scanner(System.in);
+     int a = scn.nextInt();
+     int b = scn.nextInt();
+     int c = a+b;
+     System.out.println(c);
     }
 }
